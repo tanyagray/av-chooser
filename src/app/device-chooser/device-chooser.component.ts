e2277d466f7c2fba6a1eececfa3ab7d4bbad2938
Tanya Gray <tanyag@faceme.com>
2017-05-15 21:06:44 +1200

    Correctly select default device, with fallback

--- src/app/device-chooser/device-chooser.component.ts
+++ src/app/device-chooser/device-chooser.component.ts
@@ -1,123 +1,158 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 
 import 'rxjs/add/observable/fromPromise';
 import 'rxjs/add/observable/from';
+import 'rxjs/add/operator/scan';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/filter';
-import 'rxjs/add/observable/interval';
+import 'rxjs/add/observable/timer';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/observable/combineLatest';
+import 'rxjs/add/operator/share';
+import 'rxjs/add/operator/defaultIfEmpty';
 
 @Component({
   selector: 'device-chooser',
   templateUrl: './device-chooser.component.html',
-  styleUrls: ['./device-chooser.component.scss'],
-  inputs: ['kind', 'okayStatus', 'notOkayStatus'],
-  outputs: ['okayStatus', 'notOkayStatus', 'selectedDeviceLabel']
+  styleUrls: ['./device-chooser.component.scss']
 })
 export class DeviceChooserComponent implements OnInit {
 
-  public kind: string;
-  public label: string;
-  public okayStatus: string = "Success with";
-  public notOkayStatus: string = "No success with";
+  @Input() public kind: string;
+  @Input() public okayStatus = 'Success with';
+  @Input() public notOkayStatus = 'No success with';
 
-  public selectedDeviceLabel: string = '';
+  @Output() public $selectedDevice: Subject<MediaDeviceInfo>;
+  @Output() public selectedDeviceLabel: string;
 
-  public defaultDevice: MediaDeviceInfo;
-  public devices: MediaDeviceInfo[];
-  public selectedDevice: Subject<MediaDeviceInfo>;
+  public $availableDevices: Observable<MediaDeviceInfo[]>;
+  public $defaultDevice: Observable<MediaDeviceInfo>;
 
-  public $availableDevices: Observable<MediaDeviceInfo>;
-  public $selectedDevice: Subject<MediaDeviceInfo>;
-  
+  public $deviceList: Observable<MediaDeviceInfo[]>;
 
+  constructor() {
+
+    this.$selectedDevice = new Subject();
+
+    this.$selectedDevice.subscribe( device => {
+      this.selectedDeviceLabel = device.label;
+    });
+
+    // observable of all available devices
+    this.$availableDevices = Observable.timer( 0, 3000 )
+      .flatMap( this.getAvailableDevices )
+      .map( devices => this.filterDevicesByType(devices) )
+      .distinctUntilChanged( null, this.deviceListToString )
+      .share();
+  }
 
   ngOnInit() {
-    
+
+    // observable of the array of suitable devices
+    this.$deviceList = this.$availableDevices
+      .map( devices => devices.filter(this.excludeDefaultDevice) );
+
+    // observable of the default device, if any
+    this.$defaultDevice = this.$availableDevices
+      .flatMap( this.getUniqueDevices )
+      .scan( this.scanForDefault );
+
+    // combine the device list and default device
+    // to get the default selection
+    Observable.combineLatest( this.$deviceList, this.$defaultDevice, this.findDefaultInDevices )
+      .subscribe( selected => this.setSelectedDevice(selected) );
   }
 
+  scanForDefault( acc, curr ) {
 
-  constructor() { 
+    if ( !acc ) {
+      return curr;
+    }
 
-    this.selectedDevice = new Subject();
-    this.devices = [];
+    if ( curr.groupId === acc.groupId ) {
+      acc = curr;
+    }
 
-    Observable.fromPromise( navigator.mediaDevices.enumerateDevices() )
-      .subscribe( devices => this.processDevices(devices) );
+    return acc;
 
-      // to array
-      // filter audio inputs
-      // do
-
-    this.selectedDevice
-      .subscribe( device => this.selectedDeviceChanged( device ) );
-
-
-    this.$availableDevices = Observable.interval(1000)
-      .flatMap( this.getAvailableDevices );
   }
 
-  getAvailableDevices(): Observable<MediaDeviceInfo> {
+  /**
+   * Gets all available media devices
+   */
+  getAvailableDevices(): Observable<MediaDeviceInfo[]> {
     return Observable.fromPromise( navigator.mediaDevices.enumerateDevices() );
   }
 
+  /**
+   * Reduces a list of MediaDeviceInfo objects to a
+   * string of their deviceIds separated by commas
+   */
+  deviceListToString( devices ) {
 
-  processDevices( allDevices ){
+    const toDeviceIdString = ( deviceString, currentDevice ) => {
+      return deviceString + currentDevice.deviceId + ', ';
+    };
 
-    this.devices = allDevices.filter( device => device.kind == this.kind );
-    this.selectDefaultDevice();
-
+    return devices.reduce( toDeviceIdString, '' );
   }
 
-  selectDefaultDevice() {
+  /**
+   * Splits an array of MediaDevices into individual devices
+   */
+  getUniqueDevices( deviceArray ):Observable<MediaDeviceInfo> {
+    return Observable.from( deviceArray );
+  }
 
-    this.defaultDevice = this.devices.find( device => device.deviceId == 'default');
+  /**
+   * Filters a device array for only the required type of device
+   */
+  filterDevicesByType( devices ) {
+    return devices.filter( device => device.kind === this.kind );
+  }
 
-    if( this.defaultDevice ) {
-      // remove the default from the list
-      let defaultDeviceIndex = this.devices.indexOf(this.defaultDevice);
-      this.devices.splice( defaultDeviceIndex, 1 );
+  /**
+   * This filter returns false for any default device
+   */
+  excludeDefaultDevice( device ) {
+    return device.deviceId !== 'default';
+  }
 
-    } else {
-      // use any available device
-      this.defaultDevice = this.devices[0];
+  /**
+   * This filter returns false for any default device
+   */
+  includeDefaultDevice( device ) {
+    return device.deviceId === 'default';
+  }
 
+  /**
+   * Filter array to include only the default device,
+   * or otherwise select the first alternate option
+   */
+  findDefaultInDevices(devices, defaultDevice) {
+
+    let selected;
+
+    if ( defaultDevice ) {
+      selected = devices.find( device => device.groupId === defaultDevice.groupId);
     }
 
-    // select the matching actual device
-    let selectedDevice = this.devices.find( device => device.groupId == this.defaultDevice.groupId );
-    this.setSelectedDevice( selectedDevice );
+    if ( !selected && devices.length > 0 ) {
+      selected = devices[0];
+    }
 
+    return selected;
   }
 
   setSelectedDevice(device) {
-    this.selectedDevice.next( device );
-  }
-
-
-  selectedDeviceChanged(device) {
-
-    this.selectedDeviceLabel = device.label;
-
-    // TODO: show video or audio status
-    switch( this.kind ) {
-
-      case 'audioinput':
-        console.log('connect audio input');
-        break;
-
-      case 'audiooutput':
-        console.log('connect audio output');
-        break;
-
-      case 'videoinput':
-        console.log('connect video input');
-        break;
-
-    }
+    this.$selectedDevice.next( device );
   }
 
 }
